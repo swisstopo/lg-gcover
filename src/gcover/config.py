1d0e484fd182f26a417e887396734166214c8524
Marc Mönneråt <procrastinatio@gmail.com>
2025-07-04 16:13:46 +0200

    SDE connection management (#6)
    * add SDE management class
    * basic doc, add Exception
    * basic bridge class framework
    
    ---------
    
    Co-authored-by: Monnerat Marc swisstopo <marc.monnerat@swisstopo.ch>

---
+++ src/gcover/config.py
@@ -0,0 +1,15 @@
+# gcover/config.py (exemple)
+"""Configuration pour lg-gcover"""
+
+# Instances SDE disponibles
+SDE_INSTANCES = {
+    "prod": "GCOVERP",
+    "integration": "GCOVERI",
+
+}
+
+# Versions par défaut
+DEFAULT_VERSIONS = {
+    "GCOVERP": "SDE.DEFAULT",
+    "GCOVERI": "SDE.DEFAULT"
+}
