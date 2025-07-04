9d36cb7e9faece650725eb9dc3223c4194cde223
Marc Monnerat <procrastinatio@gmail.com>
2025-07-04 14:02:42 +0200

    basic doc, add Exception

--- src/gcover/sde/__init__.py
+++ src/gcover/sde/__init__.py
@@ -1,5 +1,8 @@
 
 
 from .connection_manager  import SDEConnectionManager
+from .bridge import  import LayerBridge
+from .exceptions import SDEConnectionError, SDEVersionError
 
-__all__ = ["SDEConnectionManager"]
+
+__all__ = ["SDEConnectionManager", "LayerBridge", "SDEConnectionError", "SDEVersionError"]
