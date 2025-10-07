2cea0e8dd913b66bff49973de205a8d58feca1fd
Marc Monnerat <procrastinatio@gmail.com>
2025-10-07 17:27:24 +0200

    missing import

--- src/gcover/cli/publish_cmd.py
+++ src/gcover/cli/publish_cmd.py
@@ -25,6 +25,11 @@ from gcover.publish.esri_classification_applicator import \
     ClassificationApplicator
 from gcover.publish.style_config import (BatchClassificationConfig,
                                          apply_batch_from_config)
+
+from gcover.publish.esri_classification_extractor import extract_lyrx
+
+from gcover.publish.generator import MapServerGenerator, QGISGenerator
+
 from gcover.publish.tooltips_enricher import (EnhancedTooltipsEnricher,
                                               EnrichmentConfig, LayerMapping,
                                               LayerType,
