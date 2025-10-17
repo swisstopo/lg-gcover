444f80d23c790131f7ef701d7759aa2bebba0a81
Marc Monnerat <procrastinatio@gmail.com>
2025-10-17 18:06:48 +0200

    display extracted styles to console

--- src/gcover/publish/esri_classification_extractor.py
+++ src/gcover/publish/esri_classification_extractor.py
@@ -24,14 +24,8 @@ from rich.table import Table
 from rich.tree import Tree
 
 # Try to import arcpy - graceful degradation if not available
-try:
-    from gcover.arcpy_compat import HAS_ARCPY, arcpy
+from gcover.arcpy_compat import HAS_ARCPY, arcpy
 
-    HAS_ARCPY = True
-    logger.info("arcpy is available - full functionality enabled")
-except ImportError:
-    HAS_ARCPY = False
-    logger.warning("arcpy not available - using CIM JSON parsing only")
 
 console = Console()
 
