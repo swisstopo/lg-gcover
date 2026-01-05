3777595faf6b4ccabb0eb133c30ccc98a48f53a9
Marc Monnerat <procrastinatio@gmail.com>
2026-01-05 16:22:24 +0100

    remove publish symbols

--- src/gcover/cli/publish_cmd.py
+++ src/gcover/cli/publish_cmd.py
@@ -41,7 +41,7 @@ from gcover.publish.tooltips_enricher import (EnhancedTooltipsEnricher,
                                               LayerType,
                                               create_enrichment_config)
 
-from gcover.cli.symbols_cli import symbols_commands
+# TODO from gcover.cli.symbols_cli import symbols_commands
 
 
 
@@ -76,7 +76,7 @@ def publish_commands(ctx):
     ctx.obj.setdefault("verbose", False)
     ctx.obj.setdefault("config_path", None)
 
-publish_commands.add_command(symbols_commands)
+# TODO publish_commands.add_command(symbols_commands)
 
 
 @publish_commands.command()
