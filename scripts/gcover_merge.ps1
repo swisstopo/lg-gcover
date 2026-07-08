9a2f6296a610b4ec8b26723bb5cfe08c3dfd2690
Monnerat Marc swisstopo <marc.monnerat@swisstopo.ch>
2026-07-08 17:50:00 +0200

    simple merge script

---
+++ scripts/gcover_merge.ps1
@@ -0,0 +1,34 @@
+
+
+$gcover =   'Y:\conda\envs\ARCGIS_37\Scripts\gcover.exe'
+$DELIVERY = '\\v0t0020a.adr.admin.ch\lg\01_PRODUKTION\GIS\TOPGIS\Produktableitung\R17_2026'
+$SOURCES  = $DELIVERY + '\Sources'
+$EXCELS   = $DELIVERY + '\Excels'
+$OUTPUT   = '\\adb.intra.admin.ch\swisstopo$\Transfer\mom\R17'
+
+# $OUTPUT   = 'D:\GeoCover\R17'
+
+$env:HTTP_PROXY = 'http://proxy-bvcol.admin.ch:8080'
+$env:HTTPS_PROXY = 'http://proxy-bvcol.admin.ch:8080'
+
+
+$mergeArgs = @(
+    '--env', 'production',
+    '--config', 'H:\.config\gcover\environments\production.yaml',
+    '--verbose',
+    'publish', 'merge',
+    '--rc1',                ($SOURCES + '\20260518_0341_2016-12-31.gdb'),
+    '--rc2',                ($SOURCES + '\20260518_0330_2030-12-31.gdb'),
+    '--custom-sources-dir', $SOURCES,
+    '--sources',            ($EXCELS + '\GC_Sources_PA.xlsx'),
+    '--force-2d',
+    '--output',             ($OUTPUT + '\merged_master.gdb'),
+    '--no-clip-to-swiss-border',
+    '--enrich-mapsheet-links',
+    '--exclude-metadata',
+    '--schema-output',      ($OUTPUT + '\merged_final.gdb'),
+    '--strati-links',       ($EXCELS + '\_Update_stratiLINK.xlsx')
+)
+
+& $gcover @mergeArgs
+
